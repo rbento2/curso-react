6b5879988f5b7bff2d28567614c880ad94805372
Renato Bento <renatobento@Renatos-MacBook.local>
2019-04-03 19:08:24 -0300

    commit inicial

---
+++ src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'module/bootstrap/dist/css/bootstrap.min.css'
+
+
+import React from 'react'
+
+export default props => (
+    <div className="container">
+        <h1>TESTE</h1>
+    </div>
+)
